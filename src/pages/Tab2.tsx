d89cc5a9c73a1b9c33f46510247f67dcc089a586
Nerv3sine <44034888+Nerv3sine@users.noreply.github.com>
2021-06-05 15:51:42 -0700

    Added page content loading mechanics

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,63 @@
 import { IonCard, IonCardContent, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import RecipeSlip from '../components/RecipeSlip';
+
+const databaseInfo = {
+  "Cookbook": [
+    {
+      "Recipe": {
+        "Name": "Chocolate Cookie",
+        "Duration": "3 hours",
+        "Ingredients": [
+          { "Ingredient": "Flour" },
+          { "Ingredient": "Butter" },
+          { "Ingredient": "Chocolate Chips" }
+        ],
+        "Method": [
+          {
+            "Step": "Combine the bread crumbs, sugar, cinnamon, and nutmeg. Cut in the butter. Slowly add the water and mix."
+          },
+          { "Step": "Bake in a 350 degree F oven for 30 minutes." }
+        ],
+        "Yield": 12,
+        "Saved": true
+      }
+    },
+    {
+      "Recipe": {
+        "Name": "Soup",
+        "Duration": "1.5 hours",
+        "Ingredients": [{ "Ingredient": "Broth" }, { "Ingredient": "Noodles" }],
+        "Method": [{ "Step": "Boil the ingredients for a bit." }],
+        "Yield": 6,
+        "Saved": true
+      }
+    },
+    {
+      "Recipe": {
+        "Name": "Apple Brown Betty",
+        "Duration": "4 hours",
+        "Ingredients": [
+          { "Ingredient": "bread crumbs" },
+          { "Ingredient": "butter" },
+          { "Ingredient": "apple" },
+          { "Ingredient": "sugar" },
+          { "Ingredient": "cinnamon" },
+          { "Ingredient": "nutmeg" },
+          { "Ingredient": "water" }
+        ],
+        "Method": [
+          {
+            "Step": "Combine the bread crumbs, sugar, cinnamon, and nutmeg. Cut in the butter. Slowly add the water and mix."
+          },
+          { "Step": "Bake in a 350 degree F oven for 30 minutes." }
+        ],
+        "Yield": 6,
+        "Saved": true
+      }
+    }
+  ]
+}
 
 const Tab2: React.FC = () => {
   return (
@@ -15,21 +72,9 @@ const Tab2: React.FC = () => {
 
       <IonContent fullscreen>
         <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonCard color="warning" class="ion-no-margin">
-                <IonCardTitle class="ion-padding">
-                  Food Name
-                </IonCardTitle>
-
-                <IonCardContent>
-                  <h2>
-                    Time<br></br>Missing Ingredients
-                  </h2>
-                </IonCardContent>
-              </IonCard>
-            </IonCol>
-          </IonRow>
+          {databaseInfo.Cookbook.map((Rec) => {
+            return <RecipeSlip recipeName={Rec.Recipe.Name} recipeDuration={Rec.Recipe.Duration} missingCount={Rec.Recipe.Ingredients.length}/>
+          })}
         </IonGrid>
       </IonContent>
     </IonPage>
