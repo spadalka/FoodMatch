44fb33f7d116f6e071b2f37677cfec0112192d71
Nerv3sine <44034888+Nerv3sine@users.noreply.github.com>
2021-06-05 16:40:29 -0700

    reFormatted the RecipeSlip component

--- src/components/RecipeSlip.tsx
+++ src/components/RecipeSlip.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import {IonRow, IonCol, IonCard, IonCardTitle, IonCardContent} from '@ionic/react';
+import React from "react";
+import {
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardTitle,
+  IonCardContent,
+} from "@ionic/react";
 
-const RecipeSlip: React.FC<{recipeName: string; recipeDuration: string; missingCount: number}> = props => {
-    return (
-        <IonRow>
-            <IonCol>
-              <IonCard color="warning" class="ion-no-margin">
-                <IonCardTitle class="ion-padding">
-                  {props.recipeName}
-                </IonCardTitle>
+const RecipeSlip: React.FC<{
+  recipeName: string;
+  recipeDuration: string;
+  missingCount: number;
+}> = (props) => {
+  return (
+    <IonRow>
+      <IonCol>
+        <IonCard color="warning" class="ion-no-margin">
+          <IonCardTitle class="ion-padding">{props.recipeName}</IonCardTitle>
 
-                <IonCardContent>
-                  <h2>
-                    {props.recipeDuration}<br></br>{props.missingCount} Ingredients Involved
-                  </h2>
-                </IonCardContent>
-              </IonCard>
-            </IonCol>
-        </IonRow>
-    )
-}
+          <IonCardContent>
+            <h2>
+              {props.recipeDuration}
+              <br></br>
+              {props.missingCount} Ingredients Involved
+            </h2>
+          </IonCardContent>
+        </IonCard>
+      </IonCol>
+    </IonRow>
+  );
+};
 
 export default RecipeSlip;
